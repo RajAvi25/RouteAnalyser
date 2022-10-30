6ed93379178b3fcf5d5429068f70c126a0a4e55d
RajAvi25 <rajavi6362@gmail.com>
2022-10-30 21:33:11 +0530

    Comment

--- first.js
+++ first.js
@@ -5,3 +5,4 @@ const data_to_pass = "Send this to Python";
 console.log("Data sent to python");
 const python_process = spawner('python',['./test.py',data_to_pass]);
 python_process.stdout.on('data',(data)=>{console.log('Data received from Python:', data.toString())});
+//
